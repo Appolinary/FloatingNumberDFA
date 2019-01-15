6f3c178cff7b3b671c2c57f70448eaf54a89e830
Fabrice Appolinary <fabrice.apolinary@ds2.CS.McGill.CA>
2019-01-14 19:52:33 -0500

    first commit just added files

---
+++ src/floatingnumberdfa/FloatingNumberDFA.java
@@ -0,0 +1,14 @@
+
+package floatingnumberdfa;
+
+/**
+ *
+ * @author fabrice.apolinary
+ */
+public class FloatingNumberDFA {
+
+    public static void main(String[] args) {
+        System.out.println("Helo");
+    }
+    
+}
